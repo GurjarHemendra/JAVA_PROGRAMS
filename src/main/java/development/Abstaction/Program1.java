f515236d89a9e2a9ad7b72c0050b1a58d6c69dd4
unknown <connecthemendra29@gmail.com>
2023-10-16 21:36:29 +0530

    abstaction code

---
+++ src/main/java/development/Abstaction/Program1.java
@@ -0,0 +1,15 @@
+package development.Abstaction;
+
+public abstract class Program1 {
+
+    //abstract method
+ abstract public void test();
+
+ // can i write more than one abstract method
+
+    abstract public void demo();
+
+    //
+     abstract public  void alpha();
+
+}
